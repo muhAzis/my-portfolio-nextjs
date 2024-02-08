e82bf5337d7af14ea5b840e3f16cb52df96fe84b
Muhamad Abdul Azis <103145762+muhAzis@users.noreply.github.com>
2024-02-08 17:47:50 +0700

    + Adding Skills section + Adding image assets

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import '@/styles/page.css';
 import Hero from '@/views/Hero';
+import Skills from '@/views/Skills';
 
 export default function Home() {
   return (
     <div id="app">
       <Hero />
+      <Skills />
     </div>
   );
 }
---
+++ src/views/Skills.tsx
@@ -0,0 +1,47 @@
+import '@/styles/Skills.css';
+import React from 'react';
+
+const Skills: React.FC = () => {
+  const progIcons = ['HTML', 'CSS', 'Javascript', 'Typescript', 'Node', 'React', 'Next', 'C', 'CPP', 'Python', 'PHP', 'Golang', 'Bootstrap', 'MongoDB', 'MySQL', 'GIT', 'VS Code'];
+
+  return (
+    <main id="skills">
+      <div className="row1">
+        <h2 className="title">
+          What <span className="gradient-text">skills</span> I am good at
+        </h2>
+        <div className="skill-cards">
+          <div className="card">
+            <img src="/Code.png" alt="clean-code" className="skill-icon" />
+            <p className="skill-desc">Clean and readable code to ease the web maintain and debug</p>
+          </div>
+          <div className="card">
+            <img src="/Device.png" alt="responsive layout" className="skill-icon" />
+            <p className="skill-desc">Responsive web layout to fit the layout with various different devices</p>
+          </div>
+          <div className="card">
+            <img src="/Layout.png" alt="minimlaist design" className="skill-icon" />
+            <p className="skill-desc">Minimalist web design to please the user with an aesthetic modern design</p>
+          </div>
+        </div>
+      </div>
+      <div className="row2">
+        <h4 className="tech-title">
+          <span className="gradient-text">Technology</span> I currently use
+        </h4>
+        <div className="prog-stacks">
+          <div className="icons">
+            {progIcons.map((icon, index) => (
+              <div key={index} className="icon" style={{ height: '30px', width: 'fit-content', maxWidth: '50px' }}>
+                <img className="icon-image" src={`/${icon}.png`} alt={icon} style={{ width: '100%', objectFit: 'cover' }} />
+              </div>
+            ))}
+          </div>
+          <div className="tech-image" />
+        </div>
+      </div>
+    </main>
+  );
+};
+
+export default Skills;
