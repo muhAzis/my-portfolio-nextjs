e97e0799d3a65eb6525b2bb2e43688b81d057927
Muhamad Abdul Azis <103145762+muhAzis@users.noreply.github.com>
2024-02-09 17:54:39 +0700

    - Update responsive layout for Navbar section

--- src/views/Navbar.tsx
+++ src/views/Navbar.tsx
@@ -1,12 +1,14 @@
 'use client';
 import '@/styles/Navbar.css';
 import ButtonCTA from '@/components/ButtonCTA';
-import Image from 'next/image';
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import Logo from '@/components/Logo';
 
 const Navbar: React.FC = () => {
+  const logoBtnRef = useRef<HTMLDivElement>(null);
+
   const [yOffset, setYOffset] = useState<number>(0);
+  const [menuActive, setMenuActive] = useState<boolean>(false);
 
   useEffect(() => {
     window.addEventListener('scroll', () => {
@@ -16,28 +18,44 @@ const Navbar: React.FC = () => {
     return () => window.removeEventListener('scroll', () => {});
   }, [setYOffset]);
 
+  useEffect(() => {
+    if (logoBtnRef.current) {
+      const button = logoBtnRef.current;
+      button.onclick = () => {
+        setMenuActive(!menuActive);
+      };
+    }
+  }, [menuActive]);
+
   const handleCTA = (): void => {
     location.href = '#contacts';
   };
 
+  const handleDownloadCV = (): void => {
+    window.open('/Muh Abdul Azis CV.pdf', '_blank');
+  };
+
   return (
-    <nav id="navbar" style={yOffset > 0 ? { backdropFilter: 'blur(20px)', boxShadow: 'var(--box-shadow-lite1)' } : {}}>
+    <nav id="navbar" style={yOffset > 0 || menuActive ? { backgroundColor: 'rgba(245, 245, 245, 0.3)', backdropFilter: 'blur(20px)', boxShadow: 'var(--box-shadow-lite1)' } : {}}>
       <div className="navbar-container">
-        <div className="logo">
+        <div ref={logoBtnRef} className="logo">
           <Logo style={{ width: '50px', height: 'fit-content' }} />
         </div>
-        <ul className="menu-list">
-          <li className="menu-item">
+        <ul className={menuActive ? 'menu-list active' : 'menu-list'}>
+          <li onClick={() => setMenuActive(false)} className="menu-item">
             <a href="#hero">Home</a>
           </li>
-          <li className="menu-item">
+          <li onClick={() => setMenuActive(false)} className="menu-item">
             <a href="#skills">Skills</a>
           </li>
-          <li className="menu-item">
+          <li onClick={() => setMenuActive(false)} className="menu-item">
             <a href="#projects">Projects</a>
           </li>
-          <li className="menu-item">
+          <li onClick={() => setMenuActive(false)} className="menu-item">
             <ButtonCTA action={handleCTA}>Let&apos;s connect...</ButtonCTA>
+          </li>
+          <li onClick={() => setMenuActive(false)} id="downloadCV2" className="menu-item">
+            <ButtonCTA action={handleDownloadCV}>Download as CV</ButtonCTA>
           </li>
         </ul>
       </div>
