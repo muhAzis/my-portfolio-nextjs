d8d8fc540f4327c1241796e96bbef2d6e034652c
Muhamad Abdul Azis <103145762+muhAzis@users.noreply.github.com>
2024-02-27 02:31:22 +0700

    - Animation bug fix

--- src/views/Skills.tsx
+++ src/views/Skills.tsx
@@ -15,7 +15,7 @@ const parent2: Variants = {
 };
 
 const children: Variants = {
-  hidden: { opacity: 0, x: '50%' },
+  hidden: { opacity: 0, x: '10%' },
   visible: { opacity: 1, x: '0%', transition: { duration: 0.3, staggerChildren: 0.03 } },
 };
 
