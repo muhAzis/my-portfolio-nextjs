6543800bf453a1b402e1f4a920de41c9fc1c9173
Muhamad Abdul Azis <103145762+muhAzis@users.noreply.github.com>
2024-04-20 06:00:46 +0700

    + Adding Projects page and the individual project page + Change style file for HomePage

---
+++ src/app/projects/[projectName]/page.tsx
@@ -0,0 +1,8 @@
+import UnderDevelopment from '@/views/UnderDevelopment';
+import React from 'react';
+
+const ProjectPage = () => {
+  return <UnderDevelopment />;
+};
+
+export default ProjectPage;
---
+++ src/app/projects/page.tsx
@@ -0,0 +1,8 @@
+import UnderDevelopment from '@/views/UnderDevelopment';
+import React from 'react';
+
+const ProjectsPage = () => {
+  return <UnderDevelopment />;
+};
+
+export default ProjectsPage;
